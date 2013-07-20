95a5dc958f90f5dc80ed788d681db668d50a4a92
DC* <dc@syncr.com.ar>
2013-07-20 07:12:52 -0300

    Initial commit

---
+++ src/Asphyxia/Hydra/StaticAssets.php
@@ -0,0 +1,27 @@
+<?php
+namespace Asphyxia\Hydra;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Yaml\Yaml;
+
+
+class StaticAssets implements ServiceProviderInterface
+{
+    protected $path;
+    public function __construct($path) {
+      $this->path = $path;
+    }
+
+    public function fetch($file) {
+      return file_get_contents($this->path . '/' . $file);
+    }
+
+    public function register(Application $app) {
+       $app['static'] = $this;
+    }
+
+    public function boot(Application $app) {
+    }
+}
+?>
